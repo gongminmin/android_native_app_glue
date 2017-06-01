bd129b034fe6f8cc09f90c971dfe33df1593c55e
Dan Albert <danalbert@google.com>
2017-06-01 10:49:57 -0700

    Use -u for native_app_glue.
    This way people don't have to change makefiles that have worked until
    now (though they wouldn't anyway if they're still using `app_dummy`).
    
    Test: ./run_tests.py --rebuild --filter native_app_glue
    Bug: https://github.com/android-ndk/ndk/issues/381
    Change-Id: Id6c09cc83e462b9914c810fe79d924f1fc28e1d4

--- android_native_app_glue.h
+++ android_native_app_glue.h
@@ -337,9 +337,7 @@ void android_app_post_exec_cmd(struct android_app* android_app, int8_t cmd);
  * does this for us.
  */
 __attribute__((
-    deprecated("Calls to app_dummy are no longer necessary. Use "
-               "`LOCAL_WHOLE_STATIC_LIBRARIES := android_native_app_glue` and "
-               "remove the call. See "
+    deprecated("Calls to app_dummy are no longer necessary. See "
                "https://github.com/android-ndk/ndk/issues/381."))) void
 app_dummy();
 
